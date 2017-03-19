0f5b2de21b5628c3fce3a35231678328e9eb9edc
Murray Lucas <murray.lucas@orionhealth.com>
2017-03-19 14:26:17 +1300

    SQUASH:  swapped search for MUI Autocomplete.

--- src/components/SearchableContactList.js
+++ src/components/SearchableContactList.js
@@ -1,4 +1,6 @@
 import React, { Component, PropTypes } from 'react';
+import { AutoComplete } from 'material-ui';
+
 import ContactList from './ContactList';
 
 class SearchableContactList extends Component {
@@ -7,22 +9,35 @@ class SearchableContactList extends Component {
     this.state = {
       searchText: '',
     };
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleUpdateInput = this.handleUpdateInput.bind(this);
   }
 
-  handleSubmit() {
-    this.setState({
-      searchText: this.input.value,
-    });
+  handleUpdateInput(searchText) {
+    this.setState({ searchText });
+  }
+
+  handleNewRequest() {
+    this.setState({ searchText: '' });
   }
 
   render() {
     const { users } = this.props;
     const searchText = this.state.searchText.toLowerCase();
     const filteredUsers = users.filter(({ name }) => name.toLowerCase().indexOf(searchText) > -1);
+    const dataSource = users.map(({ name }) => name); // const.
+
+    // TODO: don't really like the autocomplete dropping over lists when its super short...
+    // maybe address this :/
     return (<div>
-      {/* TODO: change the handleSubmit function to a debounced function: */}
-      <input id="search" type="text" ref={(input) => { this.input = input; }} onChange={this.handleSubmit} />
+      <AutoComplete
+        floatingLabelText="Search"
+        filter={AutoComplete.fuzzyFilter}
+        dataSource={dataSource}
+        maxSearchResults={5}
+        searchText={this.state.searchText}
+        onUpdateInput={this.handleUpdateInput}
+        openOnFocus
+      />
       <ContactList users={filteredUsers} />
     </div>
     );
