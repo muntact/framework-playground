f8c3da541c8150fa13b99d315c2e06c5c3535a48
Murray Lucas <murray.lucas@orionhealth.com>
2017-03-23 15:14:10 +1300

    Re-tweak card

--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -1,4 +1,5 @@
 import React, { PropTypes } from 'react';
+import CardBackground from '../assets/images/card-background.png';
 
 const ContactCard = ({ user }) => {
   const { address, company, email, name, phone, website } = user;
@@ -6,33 +7,31 @@ const ContactCard = ({ user }) => {
   const { bs, catchPhrase, name: companyName } = company;
 
   return (
-    <div className="row">
-      <div className="col s12 m12 l10">
-        <div className="card deep-purple orange-text text-accent-2">
-          <div className="card-content">
-            <span className="card-title">{name}</span>
-            <div className="row" style={{ textAlign: 'left' }}>
-              <div className="col s6 m6 l6">
-                <div><b>Address:</b></div>
-                <div>{city}</div>
-                <div>{street}</div>
-                <div>{suite}</div>
-                <div>{zipcode}</div>
-              </div>
-              <div className="col s6 m6 l6">
-                <div><b>Company:</b></div>
-                <div>{companyName}</div>
-                <div>{bs}</div>
-                <div>{catchPhrase}</div>
-              </div>
-            </div>
-            <div className="card-action">
-              <a href={`mailto:${email}`}>Email</a>
-              <a href={`tel:${phone}`}>Phone</a>
-              <a href={`http://${website}`}>Website</a>
-            </div>
+    <div className="card" style={{ margin: 'auto', maxWidth: '500px' }}>
+      <div className="card-content" style={{ backgroundImage: `url(${CardBackground})` }} >
+        <div className="row">
+          <span className="card-title"><b>{name}</b></span>
+        </div>
+        <div className="row" style={{ textAlign: 'left' }}>
+          <div className="col s6 m6 l6">
+            <div><b>Address:</b></div>
+            <div>{city}</div>
+            <div>{street}</div>
+            <div>{suite}</div>
+            <div>{zipcode}</div>
+          </div>
+          <div className="col s6 m6 l6">
+            <div><b>Company:</b></div>
+            <div>{companyName}</div>
+            <div>{bs}</div>
+            <div>{catchPhrase}</div>
           </div>
         </div>
+      </div>
+      <div className="card-action">
+        <a href={`mailto:${email}`}>Email</a>
+        <a href={`tel:${phone}`}>Phone</a>
+        <a href={`http://${website}`}>Website</a>
       </div>
     </div>
   );
